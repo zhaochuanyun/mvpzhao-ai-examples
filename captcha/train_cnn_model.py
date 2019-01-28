4e2878d2956fc6de78878877f95ca5ba7e72bce0
mvpzhao@macbook <zhaochuanyun@qq.com>
2019-01-28 17:23:44 +0800

    fix
    Change-Id: If6252b08ae52e93a08d387b73223117bde6f20de

--- captcha/train_cnn_model.py
+++ captcha/train_cnn_model.py
@@ -12,7 +12,7 @@ MODEL_VIEW = '~/data/captcha/datasets/model_view.png'
 TENSOR_BOARD_LOG = '~/data/captcha/datasets/logs'
 
 
-def captcha_recognition_model(input_shape, model_name='Captcha-Recognition-Model'):
+def cnn_model(input_shape, model_name='Captcha-Recognition-Model'):
     """
     keras实现的cnn模型
     """
@@ -41,10 +41,10 @@ def train_cnn(X_train, Y_train, X_test, Y_test, epochs=10, batch_size=64, model_
     """
     训练模型
     """
-    cnn_model = captcha_recognition_model(X_train[0, :].shape)
+    model = cnn_model(X_train[0, :].shape)
 
     # Ask Keras to build the TensorFlow model behind the scenes
-    cnn_model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # visualize loss and acc in tensorboard
     tb = TensorBoard(log_dir=os.path.expanduser(TENSOR_BOARD_LOG),
@@ -58,21 +58,21 @@ def train_cnn(X_train, Y_train, X_test, Y_test, epochs=10, batch_size=64, model_
                      embeddings_metadata=None)
 
     # Train the neural network
-    cnn_model.fit(x=X_train, y=Y_train, validation_data=(X_test, Y_test), epochs=epochs, batch_size=batch_size, callbacks=[tb])
+    model.fit(x=X_train, y=Y_train, validation_data=(X_test, Y_test), epochs=epochs, batch_size=batch_size, callbacks=[tb])
 
     # Save the trained model to disk
-    cnn_model.save(os.path.expanduser(MODEL_FILENAME))
+    model.save(os.path.expanduser(MODEL_FILENAME))
 
-    preds = cnn_model.evaluate(x=X_test, y=Y_test)
+    preds = model.evaluate(x=X_test, y=Y_test)
 
     print()
     print('Loss = ' + str(preds[0]))
     print('Test Accuracy = ' + str(preds[1]))
     print()
 
-    cnn_model.summary()
+    model.summary()
 
-    plot_model(cnn_model, to_file=os.path.expanduser(model_view))
+    plot_model(model, to_file=os.path.expanduser(model_view))
 
 
 if __name__ == '__main__':
